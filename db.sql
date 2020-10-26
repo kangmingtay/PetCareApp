971f0d2f9f8e2cc313ce2aff0de8c62218812945
kangmingtay <kang.ming1996@gmail.com>
2020-10-26 13:54:29 +0800

    update db schema

--- db.sql
+++ db.sql
@@ -44,15 +44,16 @@ CREATE TABLE IF NOT EXISTS Pet_Categories (
 );
 
 CREATE TABLE IF NOT EXISTS Prefers (
-    username VARCHAR(256) REFERENCES Care_Takers(username) ON DELETE CASCADE,
+    cname VARCHAR(256) REFERENCES Care_Takers(username) ON DELETE CASCADE,
     category VARCHAR(256) REFERENCES Pet_Categories(category) ON DELETE CASCADE,
-    PRIMARY KEY (username, category)
+    PRIMARY KEY (cname, category)
 );
 
 CREATE TABLE IF NOT EXISTS Schedule (
-  cname VARCHAR(256) REFERENCES Care_Takers (username) ON DELETE CASCADE,
-  date  DATE,
-  PRIMARY KEY (cname, date)
+    cname VARCHAR(256) REFERENCES Care_Takers (username) ON DELETE CASCADE,
+    date DATE,
+    pet_count int,
+    PRIMARY KEY (cname, date)
 );
 
 CREATE TABLE IF NOT EXISTS PCS_Administrator (
@@ -63,26 +64,33 @@ CREATE TABLE IF NOT EXISTS PCS_Administrator (
 );
 
 CREATE TABLE IF NOT EXISTS Pets (
-    pet_id VARCHAR(256),
     pet_name VARCHAR(256),
     category VARCHAR(256) NOT NULL REFERENCES Pet_Categories(category) ON DELETE CASCADE,
     pname VARCHAR(256) NOT NULL REFERENCES Pet_Owners(username) ON DELETE CASCADE,
     care_req TEXT,
-    PRIMARY KEY (pname, pet_id)
+    PRIMARY KEY (pname, pet_name)
 );
 
 CREATE TABLE IF NOT EXISTS Bids (
     pname VARCHAR(256),
-    pet_id VARCHAR(256), 
+    pet_name VARCHAR(256), 
     cname VARCHAR(256),
-    date DATE,
+    start_date DATE,
+    end_date DATE,
     rating NUMERIC CHECK (rating <= 5 AND rating > 0),
     is_selected BOOLEAN,
     payment_amt NUMERIC, 
     transaction_type VARCHAR(30),
     ranking INTEGER CHECK (ranking > 0),
     review VARCHAR(256), 
-    PRIMARY KEY(pname, pet_id, cname, date),
-    FOREIGN KEY (pname, pet_id) REFERENCES Pets(pname, pet_id) ON DELETE CASCADE,
-    FOREIGN KEY (cname, date) REFERENCES Schedule(cname, date) ON DELETE CASCADE
+    PRIMARY KEY(pname, pet_name, cname, start_date, end_date),
+    FOREIGN KEY (pname, pet_name) REFERENCES Pets(pname, pet_name) ON DELETE CASCADE,
+    FOREIGN KEY (cname) REFERENCES Care_Takers(username) ON DELETE CASCADE
 );
+
+-- CREATE VIEW as Catalogue (
+--     -- displays available schedule + pet_category preference of a care taker 
+--     -- complex query to get availability of care_takers based on schedule, prefers and bids
+--     -- problem: catalogue dates between start and end dates in bids must not be included
+--     -- <cname, category, date>
+-- );
