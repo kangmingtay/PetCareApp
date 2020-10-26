a1245b6b084cc251c2baef32bc162ccea9309fcd
kangmingtay <kang.ming1996@gmail.com>
2020-10-26 14:11:22 +0800

    add login, user handlers, routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const getUser = require('../handlers/userHandler').handleGetUser;
+const createUser = require('../handlers/userHandler').handleCreateUser;
 const getAllUsers = require('../handlers/userHandler').handleGetAllUsers;
 
+router.get('/:username', (req, res) => getUser(req, res));
+router.post('/', (req, res) => createUser(req, res));
 router.get('/', (req, res) => getAllUsers(req, res));
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8888;
 
 // configure routes 
+var loginRouter = require('./routes/login.js');
 var userRouter = require('./routes/user.js');
 
-// configure middleware 
+// configure middleware
+app.use(express.static('./public'));
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
+
+// initialise routes
+app.use('/api/login', loginRouter);
 app.use('/api/users', userRouter);
 
+// console.log("hello world");
 
 app.get('/', (req, res) => {
   res.send('Hello World! Welcome to Furry Fantasy API Server!');
