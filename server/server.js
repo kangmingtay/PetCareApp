89fa0498700a5db80be46571ff5a71c097afc6f4
shannonlee98 <shanklee.lj@gmail.com>
2020-10-26 20:08:42 +0800

    merge

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8888;
 
+// configure routes
+var loginRouter = require("./routes/login.js");
+var userRouter = require("./routes/user.js");
+
+// configure middleware
+app.use(express.static("./public"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+// initialise routes
+app.use("/api/login", loginRouter);
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World! Welcome to Furry Fantasy API Server!");
@@ -80,35 +93,6 @@ app.get(
     }
   }
 );
-
-app.listen(port, () => {
-  console.log(`Furry Fantasy server listening at http://localhost:${port}`);
-});
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const port = process.env.PORT || 8888;
-
-// configure routes
-var loginRouter = require("./routes/login.js");
-var userRouter = require("./routes/user.js");
-
-// configure middleware
-app.use(express.static("./public"));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-// initialise routes
-app.use("/api/login", loginRouter);
-app.use("/api/users", userRouter);
-
-// console.log("hello world");
-
-app.get("/", (req, res) => {
-  res.send("Hello World! Welcome to Furry Fantasy API Server!");
-});
 
 app.listen(port, () => {
   console.log(`Furry Fantasy server listening at http://localhost:${port}`);
