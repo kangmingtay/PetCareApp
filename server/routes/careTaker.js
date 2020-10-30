420431fc7ccb05f9cc34d3e58ac4967682b1741c
tzw0 <e0310410@u.nus.edu>
2020-10-31 01:03:14 +0800

    added documentation in caretaker handlers

--- server/routes/careTaker.js
+++ server/routes/careTaker.js
@@ -7,7 +7,7 @@ const updatePreferences = require('../handlers/careTakerHandler').handleUpdatePr
 const createPreferences = require('../handlers/careTakerHandler').handleCreatePreferences;
 const deletePreferences = require('../handlers/careTakerHandler').handleDeletePreferences;
 const getPreferences = require('../handlers/careTakerHandler').handleGetPreferences;
-const UpsertLeavesAvailability = require('../handlers/careTakerHandler').handlerUpsertLeavesAvailability;
+const insertLeavesAvailability = require('../handlers/careTakerHandler').handlerInsertLeavesAvailability;
 const getLeaves = require('../handlers/careTakerHandler').handleGetLeaves;
 const getAvailability = require('../handlers/careTakerHandler').handleGetAvailability
 
@@ -17,7 +17,7 @@ router.get('/prefers/:username', (req, res) => getPreferences(req,res));
 router.delete('/prefers/:username', (req, res) => deletePreferences(req,res));
 router.post('/prefers/:username', (req, res) => createPreferences(req,res));
 router.put('/prefers/:username', (req, res) => updatePreferences(req,res));
-router.post('/requestDays/:username', (req, res) => UpsertLeavesAvailability(req,res));
+router.post('/requestDays/:username', (req, res) => insertLeavesAvailability(req,res));
 router.get('/leaves/:username', (req, res) => getLeaves(req,res));
 router.get('/availability/:username', (req, res) => getAvailability(req,res));
 
