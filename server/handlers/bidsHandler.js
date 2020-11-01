1f443078b403732583532edc2dcc0e4474313b38
zayn-hsh <zayn.hsh.dev@gmail.com>
2020-11-01 22:06:12 +0800

    added some awesome endpoints to sort/filter or both by anything we want

--- server/handlers/bidsHandler.js
+++ server/handlers/bidsHandler.js
@@ -1,5 +1,20 @@
 const pool = require("../db");
 
+// Possible things care taker may want to see from bids
+
+// Sort
+// Payment_amont
+// Start date(think u did this alr)
+// End date
+// Duration
+// Payment_per_day
+// Price_above_base_per_day (price per day more than the base price of pet)
+
+// Filter
+// Pet owner
+// By month
+// By year
+
 const handleGetAllBids = async (req, res) => {
     try{
         const q = 'SELECT * FROM bids';
@@ -65,7 +80,7 @@ const handleGetPetOwnerBids = async (req, res) => {
 const handleGetCaretakerBids = async (req, res) => {
     try{
         const { username } = req.params;
-        const q = `SELECT * FROM bids WHERE is_selected IS TRUE AND cname = '${username}' ORDER BY end_date DESC`; 
+        const q = `SELECT * FROM bids WHERE cname = '${username}' ORDER BY end_date DESC`; 
         const pastJobs = await pool.query(q);
         const resp = { results: pastJobs.rows };
         return res.status(200).json(resp);
@@ -77,10 +92,77 @@ const handleGetCaretakerBids = async (req, res) => {
         });
     }
 }
+
+// Possible things care taker may want to see from bids
+
+// Sort
+// Payment_amont
+// Start date(think u did this alr)
+// End date
+// Duration
+// Payment_per_day
+// Price_above_base_per_day (price per day more than the base price of pet)
+
+// Filter
+// Pet owner
+// By month
+// By year
+
+const handleGetCareTakerBidsSortByAnything = async (req, res) => {
+    try{
+        const { username, sort, order } = req.params;
+        const q = `SELECT * FROM bids WHERE cname = '${username}' ORDER BY ${sort} ${order}`; 
+        const pastJobs = await pool.query(q);
+        const resp = { results: pastJobs.rows };
+        return res.status(200).json(resp);
+    }
+    catch(err) {
+        return res.status(400).send({
+            success: false,
+            message: err.message  
+        });
+    }
+}
+
+const handleGetCareTakerBidsFilterSortByAnything = async (req, res) => {
+    try{
+        const { username, filter, by, sort, order } = req.params;
+        const q = `SELECT * FROM bids WHERE cname = '${username}' AND ${filter} = '${by}' ORDER BY ${sort} ${order}`; 
+        const pastJobs = await pool.query(q);
+        const resp = { results: pastJobs.rows };
+        return res.status(200).json(resp);
+    }
+    catch(err) {
+        return res.status(400).send({
+            success: false,
+            message: err.message  
+        });
+    }
+}
+
+const handleGetCareTakerBidsFilterByAnything = async (req, res) => {
+    try{
+        const { username, filter, by } = req.params;
+        const q = `SELECT * FROM bids WHERE cname = '${username}' AND ${filter} = '${by}'`; 
+        const pastJobs = await pool.query(q);
+        const resp = { results: pastJobs.rows };
+        return res.status(200).json(resp);
+    }
+    catch(err) {
+        return res.status(400).send({
+            success: false,
+            message: err.message  
+        });
+    }
+}
+
 module.exports = {
     handleGetAllBids,
     handleGetAllReviews,
     handleGetOneReview,
     handleGetPetOwnerBids,
-    handleGetCaretakerBids
+    handleGetCaretakerBids,
+    handleGetCareTakerBidsSortByAnything,
+    handleGetCareTakerBidsFilterByAnything,
+    handleGetCareTakerBidsFilterSortByAnything
 }
