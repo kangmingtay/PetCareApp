4995dfe8b331cdc26c5c1db0748e1250c66b9910
kangmingtay <kang.ming1996@gmail.com>
2020-11-02 03:10:55 +0800

    add trigger for caretaker

---
+++ server/triggers/trigger_caretaker.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION add_caretaker()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- inserts into caretaker whenever a full-timer / part-timer is added
+    INSERT INTO Care_Takers(cname, rating) VALUES (NEW.cname, NULL);
+        RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER create_fulltimer_trigger
+BEFORE INSERT
+ON Full_Timer
+FOR EACH ROW
+EXECUTE PROCEDURE add_caretaker();
+
+CREATE TRIGGER create_parttimer_trigger
+BEFORE INSERT
+ON Part_Timer
+FOR EACH ROW
+EXECUTE PROCEDURE add_caretaker();
