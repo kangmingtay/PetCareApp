a76b9a0a575289bef639661586b132b48fe8e0ee
kangmingtay <kang.ming1996@gmail.com>
2020-11-05 01:45:49 +0800

    rename caretaker page

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
 import theme from 'src/theme';
 import ProfilePage from './pages/ProfilePage';
-import CareTakerPage from './pages/CareTakerPage';
+import FindCareTakerPage from './pages/FindCareTakerPage';
 import PetOwnerPage from './pages/PetOwnerPage';
 import DashboardLayout from './layouts/DashboardLayout';
 import DashboardPage from './pages/DashboardPage';
@@ -37,7 +37,7 @@ const App = () => {
               element={context.isAdmin === "true" ? <AdminPage /> : <Navigate to="/app/dashboard" />} 
             />
             <Route path="dashboard" element={<DashboardPage />} />
-            <Route path="caretakers" element={<CareTakerPage />} />
+            <Route path="caretakers" element={<FindCareTakerPage />} />
             <Route path="pets" element={<PetOwnerPage />} />
             <Route path="account" element={<ProfilePage />} />
           </ProtectedRoute>
