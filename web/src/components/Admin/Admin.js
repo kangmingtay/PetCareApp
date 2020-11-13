27aa64db82b3a05e88655746d47b9c567ec30109
kangmingtay <kang.ming1996@gmail.com>
2020-11-13 15:48:14 +0800

    update layout for admin dashboard

--- web/src/components/Admin/Admin.js
+++ web/src/components/Admin/Admin.js
@@ -1,17 +1,8 @@
-import React, { Fragment, useState } from 'react';
-import { Container, makeStyles, Grid } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Pets from './Pets';
-import WorkDays from './WorkDays';
-import Months from './Months';
-import Salary from './Salary';
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core';
 import Underperform from './Underperform';
-import CaretakerTable from './CaretakerTable';
 
-const Admin = () => {
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-
+const Admin = (props) => {
   const useStyles = makeStyles(theme => ({
     root: {
       '& > *': {
@@ -24,27 +15,7 @@ const Admin = () => {
 
   return (
     <Fragment>
-      <Container maxWidth={false}>
-        <form className={classes.root} noValidate autoComplete="off">
-          <TextField
-            id="standard-basic"
-            label="Month:"
-            onChange={e => setMonth(e.target.value)}
-          />
-          <TextField
-            id="standard-basic"
-            label="Year:"
-            onChange={e => setYear(e.target.value)}
-          />
-        </form>
-        <Grid container spacing={3}>
-          <Pets month={month} year={year} />
-          <Salary month={month} year={year} />
-        </Grid>
-      </Container>
-      {/* <CaretakerTable month={month} year={year} /> */}
-      {/* <Months year={year} /> */}
-      <Underperform month={month} year={year} />
+      <Underperform month={props.month} year={props.year} />
     </Fragment>
   );
 };
--- web/src/pages/AdminPage.js
+++ web/src/pages/AdminPage.js
@@ -1,36 +1,123 @@
-import React, { useContext } from 'react';
-import { Container, makeStyles, Typography } from '@material-ui/core';
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Container, makeStyles, Typography, Grid } from '@material-ui/core';
 import Page from 'src/components/Page';
 import { UserContext } from 'src/UserContext';
 import AdminTable from '../components/Admin/AdminTable';
 import Admin from '../components/Admin/Admin';
-import CaretakerTable from '../components/Admin/CaretakerTable';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Pets from 'src/components/Admin/Pets';
+import Salary from 'src/components/Admin/Salary';
+import { DatePicker } from '@material-ui/pickers';
 
 const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
+    padding: theme.spacing(2),
+  },
+  tabs: {
+    justifyContent: "space-evenly",
+    alignContent: "center",
   }
 }));
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const AdminPage = () => {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [selectedDate, handleDateChange] = useState(new Date());
   const { context } = useContext(UserContext);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Page className={classes.root} title="Administrator">
-      <Container maxWidth={false}>
-        <Typography variant="h2" align="center">
-          Hello Admin, {context.username}
-        </Typography>
-      </Container>
-      <Admin />
+      <Grid container spacing={2} justify="center">
+        <Grid item xs={12}>
+          <Typography variant="h2" align="center">
+            Hello Admin, {context.username}
+          </Typography>
+        </Grid>
+        <Grid item xs={4} />
+        <Grid item xs={4} justify="center">
+          <DatePicker
+            className={{
+              alignContent: "center"
+            }}
+            views={["year", "month"]}
+            label="Select month"
+            openTo="month"
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+        </Grid>
+        <Grid item xs={4} />
+        <Grid item xs ={12}>
+          <Grid container spacing={3}>
+            <Pets month={selectedDate.getMonth() + 1} year={selectedDate.getFullYear()} />
+            <Salary month={selectedDate.getMonth() + 1} year={selectedDate.getFullYear()} />
+          </Grid>
+        </Grid>
+        <Grid item xs={4} />
+        <Grid item xs={4}>
+          <Container>
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+              <Tab label="Caretakers" {...a11yProps(0)} />
+              <Tab label="All Users" {...a11yProps(1)} />
+            </Tabs>
+          </Container>
+        </Grid>
+        <Grid item xs={4} />
+      </Grid>
+
+      <TabPanel value={value} index={0}>
+        <Admin month={selectedDate.getMonth() + 1} year={selectedDate.getFullYear()}/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <AdminTable />
+      </TabPanel>
       {/* <CaretakerTable /> */}
-      <AdminTable />
     </Page>
   );
 };
+
+
 
 export default AdminPage;
