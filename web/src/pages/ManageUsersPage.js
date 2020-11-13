319f799230d4752f9feb686d30f412c3cd7158e1
kangmingtay <kang.ming1996@gmail.com>
2020-11-14 03:07:57 +0800

    update manage user page

--- web/src/pages/ManageUsersPage.js
+++ web/src/pages/ManageUsersPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import { Container, makeStyles, Typography, Box } from '@material-ui/core';
 import Page from 'src/components/Page';
 import { UserContext } from 'src/UserContext';
@@ -17,39 +16,6 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
 const ManageUsersPage = () => {
   const classes = useStyles();
   const [value, setValue] = useState(0);
@@ -66,6 +32,7 @@ const ManageUsersPage = () => {
           Hello Admin, {context.username}
         </Typography>
       </Container>
+      
       <AdminTable />
     </Page>
   );
