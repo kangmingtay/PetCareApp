4789286eeefd53300fb8074765b0a64a88cf3fd7
kangmingtay <kang.ming1996@gmail.com>
2020-11-14 08:26:43 +0800

    update trigger

--- server/triggers/trigger_caretaker.sql
+++ server/triggers/trigger_caretaker.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION add_caretaker()
 RETURNS TRIGGER AS $$
 BEGIN
     -- inserts into caretaker whenever a full-timer / part-timer is added
-    INSERT INTO Care_Takers(cname, rating) VALUES (NEW.cname, NULL);
+    INSERT INTO Care_Takers(cname, rating) VALUES (NEW.cname, 2);
         RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
